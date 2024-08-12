252882fb681339584601a92022901d265f7267dc
Ayushi Rastogi <137208776+Ayushi-Rastogii@users.noreply.github.com>
2024-08-12 21:18:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ client = init_connection()
 
 # Pull data from the collection.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
+@st.cache_data(ttl=1200)
 def get_data():
     db = client["MovieRS"]
     collection = db["pklsmov"]
